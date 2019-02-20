7b3077d0992520a34397b21b7ed542ea3ea622e9
13sfaith <13sfaith@gmail.com>
2019-02-19 22:56:12 -0700

    Spring Cleaning

--- src/components/breakTimer/BreakTimer.js
+++ src/components/breakTimer/BreakTimer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Timer from '../timer/Timer.js';
+
 class BreakTimer extends Component {
   render() {
     return(
       <div>
         <p>Nik</p>
+        <Timer />
       </div>
     );
   }
