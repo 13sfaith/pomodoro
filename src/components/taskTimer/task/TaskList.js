28152a951cfa83c364f05e8e6422b4444d741ef7
13sfaith <13sfaith@gmail.com>
2019-02-20 19:17:53 -0700

    Task List center/hover styling

--- src/components/taskTimer/task/TaskList.js
+++ src/components/taskTimer/task/TaskList.js
@@ -4,17 +4,15 @@ import './TaskList.css';
 
 class TaskList extends Component {
 
-  
+
 
   render() {
     return(
       <div className="TaskList">
         {this.props.taskList.map((task, id) => {
-          if (id % 2 === 0){
-            return <div key={id} key-get={id} className="list" onClick={this.props.getTaskValue}>{task}</div>
-          } else {
-            return <div key={id} key-get={id} className="offset list" onClick={this.props.getTaskValue}>{task}</div>
-          }
+
+          return <div key={id} key-get={id} className="offset list" onClick={this.props.getTaskValue}>{task}</div>
+
         })}
       </div>
     );
